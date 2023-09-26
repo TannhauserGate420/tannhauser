3b4f19c6d8b1e15c30099a360eeab6f3962c8d2c
zuse <tannhausergate420@protonmail.com>
2023-09-26 15:18:41 +0200

    change version number

--- atomicswap/depends/config.py
+++ atomicswap/depends/config.py
@@ -2,7 +2,7 @@
 
 tannhauser = {
     "name" : "TannhauserGate",
-    "version" : "0.3.1 dev",
+    "version" : "0.3.2 dev",
     "authors" : ("iizegrim"),
     "server" : ['http://h6icwooluklzmvonhagxrf5aqo6jybooqv35x42fw6eeovyvsihnq2yd.onion:31337'],
     "server_clear" : ['https://btcswap.net:30443'],
